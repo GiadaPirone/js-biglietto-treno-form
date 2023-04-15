ec353d2bb59ba7c4a25139d84dfb9c308bd335c9
GiadaPirone <123646621+GiadaPirone@users.noreply.github.com>
2023-04-15 17:43:56 +0200

    completamento esercizio

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+document.getElementById("bigliettoCompleto").style.display = "none";
+
+let bigliettoScontato = "Biglietto Scontato";
+let bigliettoStandard = "Biglietto Standard"
+
+let carrozza = Math.floor( Math.random()* 10);
+let CP = Math.floor( Math.random()* 100000);
 
 function Prezzo(){
     let prezzoKm = 0.21;
@@ -6,17 +13,37 @@ function Prezzo(){
     
     let etàUtente = document.getElementById("età").value;
     
+    
     if (etàUtente == "minorenne"){
         prezzoScontato = prezzoBiglietto - ( prezzoBiglietto * 20 / 100);
         document.getElementById("risultato").innerText = prezzoScontato.toFixed(2);
+        
+        // biglietto   scontato.
+        document.getElementById("risultatoOfferta").innerText = bigliettoScontato;
+        
 
     } else if ( etàUtente == "over65"){
         prezzoScontato = prezzoBiglietto - ( prezzoBiglietto * 40 / 100);
         document.getElementById("risultato").innerText = prezzoScontato.toFixed(2);
 
+        // biglietto   scontato.
+        document.getElementById("risultatoOfferta").innerText = bigliettoScontato;
+
     } else if ( etàUtente == "maggiorenne"){
         document.getElementById("risultato").innerText = prezzoBiglietto.toFixed(2);
+
+        // biglietto bigliettoStandard.
+        document.getElementById("risultatoOfferta").innerText = bigliettoStandard;
     }
+
+    // nome e cognome
+    let nome = document.getElementById("nomeCognome").value;
+    document.getElementById("risultatoNome").innerText = nome
+
+
+    document.getElementById("risultatoCarrozza").innerText = carrozza;
+    document.getElementById("risultatoCp").innerText = CP;
     
+    document.getElementById("bigliettoCompleto").style.display = "block";
 }
 
